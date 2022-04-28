4b67538114b2bbd12d4637d70a4f68a1c8a392e3
LaFusew <antoine.oddoz@gmail.com>
2022-04-28 10:21:33 +0200

    [+] entities

---
+++ api/models/Comment.go
@@ -0,0 +1,95 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Comment struct {
+	gorm.Model // extends Model that contains ID, CreatedAt, UpdatedAt, DeletedAt
+	Body      string    `gorm:"size:24;not null;unique" json:"body"`
+	Author    User      `json:"author"`
+	AuthorID  uint32    `gorm:"not null" json:"author_id"`
+	Post      Post      `json:"post"`
+	PostID    uint32    `gorm:"not null" json:"post_id"`
+}
+
+func (c *Comment) Prepare() {
+	c.ID = 0
+	c.Body = html.EscapeString(strings.TrimSpace(c.Body))
+	c.Post = Post{}
+	c.Author = User{}
+	c.CreatedAt = time.Now()
+}
+
+func (c *Comment) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "update":
+	default:
+		if c.Body == "" {
+			return errors.New("required body")
+		}
+	}
+
+	return nil
+}
+
+func (c *Comment) SaveTeam(db *gorm.DB) (*Comment, error) {
+	err := db.Debug().Create(&c).Error
+	if err != nil {
+		return &Comment{}, err
+	}
+
+	return c, nil
+}
+
+func (c *Comment) FindAll(db *gorm.DB) (*[]Comment, error) {
+	comments := []Comment{}
+	err := db.Debug().Model(&Comment{}).Limit(100).Find(&comments).Error
+	if err != nil {
+		return &[]Comment{}, err
+	}
+
+	if len(comments) > 0 {
+		for i := range comments {
+			err := db.Debug().Model(&User{}).Where("id = ?", comments[i].AuthorID).Take(&c.Author).Error
+			if err != nil {
+				return &[]Comment{}, nil
+			}
+
+			err = db.Debug().Model(&Post{}).Where("id = ?", comments[i].PostID).Take(&c.Post).Error
+			if err != nil {
+				return &[]Comment{}, nil
+			}
+		}
+	}
+	return &comments, nil
+}
+
+func (c *Comment) FindByID(db *gorm.DB, cid uint32) (*Comment, error) {
+	err := db.Debug().Model(Comment{}).Where("id = ?", cid).Take(&c).Error
+	if err != nil {
+		return &Comment{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &Comment{}, errors.New("comment not found")
+	}
+	return c, nil
+}
+
+func (c *Comment) DeleteComment(db *gorm.DB, cid uint32) (int64, error) {
+	db = db.Debug().Model(&Comment{}).Where("id = ?", cid).Take(&Comment{}).Delete(&Comment{})
+
+	if db.Error != nil {
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Comment not found")
+		}
+		return 0, db.Error
+	}
+
+	return db.RowsAffected, nil
+}
---
+++ api/models/Team.go
@@ -0,0 +1,96 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Team struct {
+	gorm.Model // extends Model that contains ID, CreatedAt, UpdatedAt, DeletedAt
+	Name           string    `gorm:"size:24;not null;unique" json:"name"`
+	Desc           string    `gorm:"size:255;" json:"desc"`
+	ProfilePicture string    `gorm:"size:255;" json:"profile_picture"`
+	IsPrivate      bool      `gorm:"default:false" json:"is_private"`
+	Members []*User `gorm:"many2many:membership"`
+}
+
+func (t *Team) Prepare() {
+	t.ID = 0
+	t.Name = html.EscapeString(strings.TrimSpace(t.Name))
+	t.Desc = html.EscapeString(strings.TrimSpace(t.Desc))
+	t.CreatedAt = time.Now()
+	t.UpdatedAt = time.Now()
+}
+
+func (t *Team) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "update":
+	default:
+		if t.Name == "" {
+			return errors.New("required name")
+		}
+	}
+
+	return nil
+}
+
+func (t *Team) SaveTeam(db *gorm.DB) (*Team, error) {
+	err := db.Debug().Create(&t).Error
+	if err != nil {
+		return &Team{}, err
+	}
+
+	return t, nil
+}
+
+func (t *Team) FindAll(db *gorm.DB) (*[]Team, error) {
+	teams := []Team{}
+	err := db.Debug().Model(&Team{}).Limit(100).Find(&teams).Error
+	if err != nil {
+		return &[]Team{}, err
+	}
+
+	return &teams, nil
+}
+
+func (t *Team) FindByID(db *gorm.DB, tid uint32) (*Team, error) {
+	err := db.Debug().Model(Team{}).Where("id = ?", tid).Take(&t).Error
+	if err != nil {
+		return &Team{}, err
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return &Team{}, errors.New("Team not found")
+	}
+	return t, nil
+}
+
+func (t *Team) UpdateTeam(db *gorm.DB, tid uint32) (*Team, error) {
+	err := db.Debug().Model(&Team{}).Where("id = ?", tid).Take(&Team{}).Updates(Team{
+		Name:           t.Name,
+		Desc:           t.Desc,
+		ProfilePicture: t.ProfilePicture,
+		IsPrivate:      t.IsPrivate,
+	}).Error
+	if err != nil {
+		return &Team{}, err
+	}
+
+	return t, nil
+}
+
+func (t *Team) DeleteTeam(db *gorm.DB, tid uint32) (int64, error) {
+	db = db.Debug().Model(&Team{}).Where("id = ?", tid).Take(&Team{}).Delete(&Team{})
+
+	if db.Error != nil {
+		if gorm.IsRecordNotFoundError(db.Error) {
+			return 0, errors.New("Team not found")
+		}
+		return 0, db.Error
+	}
+
+	return db.RowsAffected, nil
+}
