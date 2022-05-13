444072aa515de85fdca480adebd30f4390b10230
LaFusew <antoine.oddoz@gmail.com>
2022-05-13 14:03:29 +0200

    [fix] "/" test route

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -7,5 +7,5 @@ import (
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
-	responses.JSON(w, http.StatusOK, "Welcome To This Awesome API")
+	responses.JSON(w, http.StatusOK, "Ma'ter: GOB_2022")
 }
